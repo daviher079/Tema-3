db887892f773c5be53b2ab212b7fd92d27ff5783
david <daviher079@gmail.com>
2021-10-21 18:44:28 +0000

    Ejercicio6

--- codigo/Tarea06.php
+++ codigo/Tarea06.php
@@ -19,7 +19,10 @@
         $arrayDavid=array(
             "Zamora CF" => array(
                 "UDS Salamanca" => array(
-                      
+                    "Resultado" => "2 - 3",
+                    "T. Rojas" => 1,
+                    "T. Amarilla" => 0,
+                    "Penaltis" => 2 
                 ),
                 "Real Valladolid" => array(
                     "Resultado" => "2 - 1",
@@ -54,23 +57,98 @@
                     "Penaltis" => 0 
                 )
             ),
+            "Real Valladolid" => array(
+                "Zamora" => array(
+                    "Resultado" => "1 - 2",
+                    "T. Rojas" => 1,
+                    "T. Amarilla" => 0,
+                    "Penaltis" => 2 
+                ),
+                "Salamanca" => array(
+                    "Resultado" => "1 - 0",
+                    "T. Rojas" => 0,
+                    "T. Amarilla" => 2,
+                    "Penaltis" => 2 
+                ),
+                "Numancia" => array(
+                    "Resultado" => "1 - 0",
+                    "T. Rojas" => 0,
+                    "T. Amarilla" => 2,
+                    "Penaltis" => 0 
+                )
+            ),
+
+            "Numancia" => array(
+                "Zamora" => array(
+                    "Resultado" => "2 - 0",
+                    "T. Rojas" => 1,
+                    "T. Amarilla" => 0,
+                    "Penaltis" => 2
+                ),
+                "Salamanca" => array(
+                    "Resultado" => "0 - 0",
+                    "T. Rojas" => 3,
+                    "T. Amarilla" => 2,
+                    "Penaltis" => 0 
+                ),
+                "Real Valladolid" => array(
+                    "Resultado" => "0 - 1",
+                    "T. Rojas" => 0,
+                    "T. Amarilla" => 2,
+                    "Penaltis" => 0  
+                )
+            ),
 
         );
 
+        echo "<table>";
+        echo "<thead>";
+        //Hago la cabecera
+        echo "<td>Equipos</td>";
+        foreach ($arrayDavid as $keyLocales => $valueLocales) {
+            
+                echo "<td>";
+                    echo $keyLocales;
+                echo"</td>";
+        }
+        echo "</thead>";
+        //Pinto los resultados y para eso recorro el array grande
+
+        foreach ($arrayDavid as $key => $value) {
+            echo "<tr>";
+                echo"<td>",$key,"</td>" ;
+                foreach($arrayDavid[$key] as $key2 => $value2){
+                    
+                    echo "<td>",$key2,"</td>";
+                }
+            echo "</tr>";    
+        }
 
 
 
 
+        echo"</table>";
+       
+       
+        
 
-        /*$equipos=array("Equipos", "Zamora CF", "UDS Salamanca", "Real Valladolid", "Numancia");
+
+
+        /*$equipos=array( "Zamora CF", "UDS Salamanca", "Real Valladolid", "Numancia");
 
         $tabla=array();
         echo "<table style='border: solid; padding: 20px;'>";
+        
         for ($i=0; $i <count($equipos) ; $i++) { 
             $tabla[$i]=array();
             echo "<tr style='border: solid;'>";
-            for ($j=0; $j < count($equipos) ; $j++) { 
-                if($i==0)
+            for ($j=0; $j < count($equipos) ; $j++) 
+            {
+                if($i==0 &$j==0)
+                {
+                    echo "<td style='padding: 20px;'>",$tabla[$i][$j]="Equipos", "</td>";
+                } 
+                elseif($i==0)
                 {
                     echo "<td style='padding: 20px;'>",$tabla[$i][$j]=$equipos[$j], "</td>";
                 }elseif($j==0)
