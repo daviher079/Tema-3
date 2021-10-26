eeb89f1e4feb58e65d7fb72c8c0ea11b08fefe01
david <daviher079@gmail.com>
2021-10-26 17:39:14 +0000

    Ejercicio2

--- codigo/funcionesEj1-2.php
+++ codigo/funcionesEj1-2.php
@@ -20,20 +20,55 @@
         return basename($_SERVER['SCRIPT_FILENAME']);
     }
 
-    $letra=array('T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E');
-
+    
     function letraDni($numero)
     {
-        $letra=$numero%23;
-
-        for ($i=0; $i <count($letra) ; $i++) { 
+        $letra=array('T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E');
+        $resultado=$numero%23;
+         for ($i=0; $i <count($letra) ; $i++) { 
             
-            if($letra==$i)
-            {
-                return $letra[$i];
-            }
+             if($resultado==$i)
+             {
+                 return $letra[$i];
+             }
         }
     }
+
+    function generaNumeros(&$array, $min, $max,$nGenerados, $repeticion)
+    {
+        $prueba=0;
+        if($repeticion==true)
+        {
+            
+            for ($i=0; $i < $nGenerados ; $i++) 
+            { 
+                $numero=rand($min, $max);
+                
+                /*do{
+                    $array[$i]=$numero;
+                }while(array_search($numero, $array)==false);
+                */
+            }
+
+            
+        }
+            
+        
+        else
+        {
+            for ($i=0; $i < $nGenerados ; $i++) 
+            { 
+                array_push($array, rand($min, $max));
+                
+            }
+            
+        }
+        
+        return $array;
+    }
+
+
+    
 
     
 
--- codigo/indexFuncionesEj1-2.php
+++ codigo/indexFuncionesEj1-2.php
@@ -16,17 +16,36 @@
     <main>
         
         <?php
-          
+            //Ejercicio 1
             require_once("./funcionesEj1-2.php");
 
-            h1("Prueba de h1");
+            h1("Ejercicio 1");
             br();
             br();
-            p("Prueba de p");
+            p("Prueba h1 ");
+            br();
+            br();
+            p("Prueba de p ");
             br();
             br();
             echo self();
 
+            br();
+            br();
+            echo "La letra del DNI 12345678 es ",letraDni(12345678);
+             
+            //Ejercicio2
+            h1("Ejercicio 2");
+            $array=array();
+            $min=5;
+            $max=20;
+            $nGenerados=8;
+            $repeticion=true;
+
+            $array2=generaNumeros($array, $min, $max,$nGenerados, $repeticion);
+            echo "<pre>";
+                print_r($array2);
+            echo"</pre>";
 
 
         ?>
