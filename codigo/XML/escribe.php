011310a576f91d23b483b25dcf9e5615c86d79b1
david <daviher079@gmail.com>
2021-11-16 19:43:51 +0000

    Modifica xml

--- codigo/XML/escribe.php
+++ codigo/XML/escribe.php
@@ -1,10 +1,40 @@
 <?php
-    $XML= new DOMDocument("1.0", "utf-8");
-    $XML->formatOutput =true;
+    $xml= new DOMDocument("1.0", "utf-8");
+    $xml->formatOutput =true;
 
-    $ElementoRaiz=$XML->createElement("Deportes");
+    $ElementoRaiz=$xml->createElement("Deportes");
 
     $raiz=$xml->appendChild($ElementoRaiz);
-    $XML->save("./ficheros/deportes.xml");
+
+    $deporte=$xml->createElement('Deporte');
+
+    $ElementoRaiz->appendChild($deporte);
+
+    $nombre=$xml->createElement("Nombre");
+    $deporte->appendChild($nombre);
+    $texto=$xml->createTextNode("Futbol");
+    $nombre->appendChild($texto);
+
+    $jugadores = $xml->createElement("Jugadores", "22");
+    $deporte->appendChild($jugadores);
+
+    $deporte=$xml->createElement('Deporte');
+
+    $ElementoRaiz->appendChild($deporte);
+
+    $nombre=$xml->createElement("Nombre");
+    $deporte->appendChild($nombre);
+    $texto=$xml->createTextNode("Tenis");
+    $nombre->appendChild($texto);
+
+    $jugadores = $xml->createElement("Jugadores", "2");
+    $deporte->appendChild($jugadores);
+
+
+
+    $xml->save("./ficheros/deportes.xml");
+
+    
+
 
 ?>
---
+++ codigo/XML/modificar.php
@@ -0,0 +1,82 @@
+<?php
+
+    echo "<h1>Modificando archivo <i>escribeXML.php</i></h1>";
+
+    $rutaFichero = "./ficheros/deportes.xml";
+
+    // Compruebo que existe
+
+    if(file_exists($rutaFichero))
+
+    {
+        // Transforma el xml en un objeto de tipo simplexml
+
+        $xml = simplexml_load_file($rutaFichero);
+    }
+
+    else
+
+    {
+        exit();
+    }
+
+
+
+    // Reogemos el objeto de tipo dom del xml
+
+    $dom = dom_import_simplexml($xml)->ownerDocument;
+
+    // Recojo el primer hijo (Deportes)
+
+    $deportes = $dom->firstChild;
+
+    // Recorro el xml
+
+    foreach($deportes->childNodes as $deporte)
+    {
+
+        // Si recojo un deporte (y no un espacio)
+
+        if($deporte->nodeName == "Deporte")
+        {
+            // Por cada hijo
+            foreach($deporte->childNodes as $hijos)
+            {
+
+                // Si el hijo es 'Nombre'
+                if($hijos->nodeName == "Nombre")
+                {
+
+                    // Si el valor de Nombre es 'Tenis'
+                    if($hijos->nodeValue == "Tenis")
+                    {
+                        $aux = $hijos;
+
+                        do
+                        {
+                            $aux = $aux->nextSibling->nextSibling;
+                        }while(!$aux->nodeName == "Jugadores");
+
+                        // modifico el valor del nodo
+
+                        $aux->nodeValue = 2;
+
+                    }
+
+                }
+
+            }
+
+
+
+        }
+
+    }
+
+
+
+    // Se guardan los cambios
+
+    $dom->save($rutaFichero);
+
+?>
